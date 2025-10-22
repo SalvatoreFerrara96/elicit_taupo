ac1ea16052741eb483f3984c44ec63b629a8a5e0
SalvatoreFerrara96 <salvatore.ferrara-ssm@unina.it>
2025-10-22 11:00:18 +0200

    Update createWebformDict.py

--- ELICITATIONS/Cotopaxi/createWebformDict.py
+++ ELICITATIONS/Cotopaxi/createWebformDict.py
@@ -4,7 +4,7 @@ datarepo = 'local'
 # datarepo = 'github'
 
 # Github repository for answers, used only when datarepo='local' or 'local_github'
-RepositoryData = 'elicitation_taupo_answers'
+RepositoryData = 'SalvatoreFerrara96/elicitation_taupo_answers'
 
 # Parameters used only when datarepo='local' or 'local_github'
 # user = ''
